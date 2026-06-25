737c274ad16d501b7300c8adc1bacddb56ebc5b0
zhukockijmaxim9 <zhukockijmaxim@gmail.com>
2026-06-25 15:22:20 +0400

    feat: настроить переменные, файлы стилей

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './styles/variables.scss';
+import './index.scss';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
